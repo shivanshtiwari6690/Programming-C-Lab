755f14b9025f1c023502d1c88e984c3b2fe7d1e4
Shivansh Tiwari <shivanshtiwari6690@gmail.com>
2026-09-10 20:40:22 +0530

    Add files via upload

---
+++ 123/helloworld.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    // This is my first program! :)
+
+    /*
+    This
+    is 
+    also
+    a comment
+    */
+
+    printf("Hello, World!\n");
+    printf("Hope you are fine \n");
+
+
+    return 0;
+    
+}
