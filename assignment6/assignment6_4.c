a353267b59390b284f41179f77dbd98213992a45
Shivansh Tiwari <shivanshtiwari6690@gmail.com>
2026-09-15 11:36:10 +0530

    Add files via upload

---
+++ assignment6/assignment6_4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0) {
+            sum = sum + i;
+        }
+    }
+
+    if (sum == n) {
+        printf("%d is a perfect number.\n", n);
+    } else {
+        printf("%d is not a perfect number.\n", n);
+    }
+
+    return 0;
+}
